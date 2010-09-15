7c2be087d1782403bbc0b93de5bc61c18442b90a
bosh <ohwaitnvm@gmail.com>
2010-09-15 16:05:09 -0400

    forgot to change sound path. now its horribly slow every time it needs to make a sound...

--- game/Platform.java
+++ game/Platform.java
@@ -34,7 +34,7 @@ public class Platform extends BufferedApplet {
    public void resetLevelScore() { levelScore = 0; }
    public int getLevelScore() { return levelScore; }
    public int getTotalScore() { return totalScore; }
-   public void pop() { playClip("clips/pop.wav"); }
+   public void pop() { playClip("http://dl.dropbox.com/u/1737130/clips/pop.wav"); }
 
    // THE THREE MAIN CALLBACKS THAT CAN BE OVERRIDDEN BY THE APPLICATION
    public void setup() {}               // WHERE TO DECLARE THINGS
